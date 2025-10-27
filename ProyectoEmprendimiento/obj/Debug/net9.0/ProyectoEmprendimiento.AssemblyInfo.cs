66e7391b1407f3eac08cd8ede78e4cc6b908aa0e
MaxGalant <fraylingay@gmail.com>
2025-10-27 11:43:13 -0400

    ¨1¨

--- ProyectoEmprendimiento/obj/Debug/net9.0/ProyectoEmprendimiento.AssemblyInfo.cs
+++ ProyectoEmprendimiento/obj/Debug/net9.0/ProyectoEmprendimiento.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoEmprendimiento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35e04c197b711f50957fca56a6a00f2cca5f3b08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88990ce45f8cb805de231168080e4c3e30b67b1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoEmprendimiento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoEmprendimiento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
